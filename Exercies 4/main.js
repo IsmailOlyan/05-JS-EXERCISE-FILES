9799c755aed54a87965fb18284b08f5a43ec6a7e
IsmailOlyan <ismaaciil1432@gmail.com>
2026-06-12 15:10:03 +0300

    exercise 4

---
+++ Exercies 4/main.js
@@ -0,0 +1,26 @@
+function add(){
+    console.log(2+2);
+    
+} add()
+
+function subtract(){
+    console.log(10-5)
+} subtract()
+
+function multiply(){
+console.log(3*5)
+} multiply()
+
+// function expression 
+
+const greeting = function(name){
+    console.log("Hello," + name)
+}
+    greeting("Ismail Olyan")
+
+let kudar = function(x,y){
+    return x+y;
+} 
+    let result = kudar(15,5)
+    console.log(result)
+
