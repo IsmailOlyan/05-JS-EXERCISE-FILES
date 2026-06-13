c063cd98b27e697cb845e3945db8644c56b64548
IsmailOlyan <ismaaciil1432@gmail.com>
2026-06-13 11:39:17 +0300

    updated file

---
+++ Exercise 5/main.js
@@ -0,0 +1,12 @@
+  const add = (x, y) => {
+            return x + y
+        }
+        let result = add(99, 1)
+        console.log(result);
+
+
+        const division = function (c, d) {
+            return c / d
+        }
+        let natiijo = division(100, 2)
+        console.log(natiijo)
