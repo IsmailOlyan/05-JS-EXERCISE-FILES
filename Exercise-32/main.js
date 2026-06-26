9dd9d22aec1b65907cfcf517b57a032cf126d33e
IsmailOlyan <ismaaciil1432@gmail.com>
2026-06-26 17:56:29 +0300

    worked exercise files

---
+++ Exercise-32/main.js
@@ -0,0 +1,7 @@
+// let querySelect = document.querySelector('#querySelect')
+
+let query = document.querySelector('#querySelect')
+console.log('this is', querySelect);
+
+let selectAll = document.querySelectorAll(".querySelectAll")
+console.log('this is', selectAll);
